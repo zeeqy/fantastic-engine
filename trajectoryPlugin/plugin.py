5c8cae61d0646d8b03755043a6c18f8160b7844f
zeeqy <hgzhiqi@gmail.com>
2019-02-22 18:16:47 -0500

    data loader and tensor

--- trajectoryPlugin/plugin.py
+++ trajectoryPlugin/plugin.py
@@ -63,12 +63,14 @@ class API:
 		self.traject_matrix = np.empty((y_train_tensor.size()[0],0))
 
 	def dataLoader(self, train_loader, valid_loader):
-		self.batch_size = train_loader.batch_size
-		self.train_dataset = train_loader.dataset
-		self.valid_dataset = valid_loader.dataset
 		self.weight_tensor = torch.from_numpy(np.ones_like(train_loader.dataset.tensors[1],dtype=np.float32))
 		self.weight_tensor.requires_grad = False
-		self.reweight_loader = Data.DataLoader(dataset=train_loader.dataset, batch_size=self.batch_size, shuffle=False)
+		self.batch_size = train_loader.batch_size
+		self.train_dataset = train_loader.dataset
+		self.train_dataset.tensors += (self.weight_tensor,)
+		self.valid_dataset = valid_loader.dataset
+		self.train_loader = Data.DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=True)
+		self.reweight_loader = Data.DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=False)
 		self.traject_matrix = np.empty((train_loader.dataset.tensors[1].size()[0],0))
 
 	def log(self, msg, level):
