2b0c46320f9dec1caac8c82233ddf0fdcb091a35
Zhiqi Huang <Charles.hzq@gmail.com>
2019-03-22 17:48:36 -0400

    new similarity schema

--- trajectoryPlugin/plugin.py
+++ trajectoryPlugin/plugin.py
@@ -90,7 +90,7 @@ class API:
 				self.train_dataset,
 				self.weightset
 			),
-			batch_size=self.batch_size, shuffle=True,collate_fn=self._collateFn)
+			batch_size=self.batch_size, shuffle=True, collate_fn=self._collateFn)
 		self.reweight_loader = Data.DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=False)
 		self.traject_matrix = np.empty((self.train_dataset.__len__(),0))
 
@@ -121,7 +121,7 @@ class API:
 			self.traject_matrix = np.append(self.traject_matrix,np.matrix(prob_output).T,1)
 
 	def _validGrad(self, validNet):
-		valid_grad = []
+		valid_grads = []
 		validNet.eval()
 		validNet.zero_grad()
 		for step, (data, target) in enumerate(self.reweight_loader):
@@ -131,12 +131,13 @@ class API:
 			valid_loss.backward()
 		for w in validNet.parameters():
 			if w.requires_grad:
-				valid_grad.extend(list(w.grad.cpu().detach().numpy().flatten()))
-		return np.array(valid_grad)
+				valid_grads.extend(list(w.grad.cpu().detach().numpy().flatten()))
+		return np.array(valid_grads)
 
 
 	def reweightData(self, validNet, num_sample, special_index=[]):
-		valid_grad = self._validGrad(validNet)
+		valid_grads = self._validGrad(validNet)
+		sim_dict = {}
 		for cid in range(self.num_cluster):
 			subset_grads = []
 			cidx = (self.cluster_output==cid).nonzero()[0].tolist()
@@ -157,19 +158,35 @@ class API:
 			for w in validNet.parameters():
 				if w.requires_grad:
 					subset_grads.extend(list(w.grad.cpu().detach().numpy().flatten()))
-			subset_grads = np.array(subset_grads)
-			sim = 1 - spatial.distance.cosine(valid_grad, subset_grads)
+			
+			# customized similarity
+			sim = np.dot(valid_grads, subset_grads) / sample_size
+			sim_dict.update({cid : sim})
+		
+		#normalize similarity
+		sim_factor = 1.0/np.mean(np.abs(list(sim_dict.values())))
+		for key in sim_dict:
+			sim_dict[key] = sim_dict[key] * sim_factor
 
-			self.weight_tensor[cidx] += 0.05 * sim # how to update weight?
-			self.weight_tensor[cidx] = self.weight_tensor[cidx].clamp(0.001)
+		#update weights
+		for cid in range(self.num_cluster):
+			cidx = (self.cluster_output==cid).nonzero()[0].tolist()
+			self.weight_tensor[cidx] += 0.05 * sim_dict[cid]
+			
+			#print some insights about noisy data
 			if special_index != []:
 				num_special = self._specialRatio(cidx,special_index)
-				self.log('| - ' + str({cid:cid, 'size': size, 'sim': '{:.4f}'.format(sim), 'num_special': num_special, 'spe_ratio':'{:.4f}'.format(num_special/size)}),2)
+				self.log('| - ' + str({cid:cid, 'size': sample_size, 'sim': '{:.4f}'.format(sim), 'num_special': num_special, 'spe_ratio':'{:.4f}'.format(num_special/sample_size)}),2)
 			else:
-				self.log('| - ' + str({cid:cid, 'size': size, 'sim': sim}),2)
-		norm_fact = self.weight_tensor.size()[0] / torch.sum(self.weight_tensor) # normalizing weights
+				self.log('| - ' + str({cid:cid, 'size': sample_size, 'sim': sim}),2)
+
+		#normalize weight tensor
+		self.weight_tensor = self.weight_tensor.clamp(0.001)
+		norm_fact = self.weight_tensor.size()[0] / torch.sum(self.weight_tensor)
 		self.weight_tensor = norm_fact * self.weight_tensor
 		self.weightset = Data.TensorDataset(self.weight_tensor)
+		
+		#refresh train_loader
 		self.train_loader = Data.DataLoader(
 			ConcatDataset(
 				self.train_dataset,
