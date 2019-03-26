160020cea8d202c7fb29955b6114683f8b6d00be
Zhiqi Huang <zhiqihuang@gypsum.cm.cluster>
2019-03-26 00:04:28 -0400

    server sync up

--- bash.sh
+++ bash.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=experiment
 #SBATCH --output=res_output.txt  # output file
 #SBATCH -e res_bug.err        # File to which STDERR will be written
-#SBATCH --partition=1080ti-short # Partition to submit to 
-#SBATCH --mem-per-cpu=4000   # Memory in MB per cpu allocated
+#SBATCH --partition=titanx-long # Partition to submit to 
+#SBATCH --mem-per-cpu=4096   # Memory in MB per cpu allocated
 
 echo $(date -u) "Main Engine Ignition!"
 
-python3 MNIST_Baseline.py --seed=-1 --epochs=5 --burn_in=3
+python3 mnist_baseline.py --seed=-1 --epochs=80 --burn_in=10 --reweight_interval=5 --num_cluster=6 --momentum=0.9 --valid_size=5000
 
 echo $(date -u) "Mission Completed!"
