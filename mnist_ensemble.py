e1260a26428f6521d068a1bebc96279532277403
Zhiqi Huang <Charles.hzq@gmail.com>
2019-07-19 21:55:02 -0400

    check lr

--- mnist_ensemble.py
+++ mnist_ensemble.py
@@ -192,6 +192,7 @@ def main():
 	optimizer_reweight = optim.SGD(model_reweight.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=1e-4)
 	optimizer_reweight.load_state_dict(optimizer_standard.state_dict())
 	scheduler_reweight = torch.optim.lr_scheduler.StepLR(optimizer_reweight, step_size=1, gamma=0.95, last_epoch=args.burn_in-1)
+	scheduler_reweight.step()
 	epoch_reweight = []
 	epoch_trajectory = []
 
